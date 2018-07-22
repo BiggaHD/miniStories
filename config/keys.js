d885e0fb20eb61320075eafb6a8b7569ed53da30
BiggaHD <dzenis.hankusic@gmail.com>
2018-07-22 03:22:02 +0200

    Adding session authentication

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// In ordeer to use this app you need to set up the following keys
+module.exports = {
+  mongoURI:'mongodb://CHANGEME',    //mLab URI
+  googleClientID:'CHANGEME',        // Google Client ID
+  googleClientSecret:'CHANGEME'     // Google Client Secret
+}
