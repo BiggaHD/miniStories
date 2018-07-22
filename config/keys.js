308e08657a38e4792a2265cb8cd416c33c51684d
BiggaHD <dzenis.hankusic@gmail.com>
2018-07-22 03:39:25 +0200

    Adding navigation, routes, story-form

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
+// If you want to use this app you'll need to set up your keys here
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
+  mongoURI:'mongodb://CHANGEME',    // mLab URI
+  googleClientID:'CHANGEME',        // Google Client ID
+  googleClientSecret:'CHANGEME'     // GoogleClient Secret
 }
