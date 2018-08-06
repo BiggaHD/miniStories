b8299364d6bb40f533e7f949e88d31d2bd2fee7c
BiggaHD <dzenis.hankusic@gmail.com>
2018-08-06 04:44:34 +0200

    heroku deploy

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,9 @@
+// If you want to use this app you'll need to set up your keys here
 module.exports = {
-  mongoURI:process.env.MONGO_URI,
-  googleClientID:process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET
+  // mLab URI
+  mongoURI:'mongodb://BiggaHD:Test123@ds113482.mlab.com:13482/mini_stories',
+  // Google Client ID
+  googleClientID:'172280978657-h7hjf5gacgj047gsa3ra7gjfc3s0iprk.apps.googleusercontent.com',
+  // GoogleClient Secret
+  googleClientSecret:'B-FubQopFFamfNfeOhPD0hXn'     
 }
