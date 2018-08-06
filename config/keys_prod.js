77b2d2d36f5c1f94e06a127f7e73b8b818918008
Dzenis H <dzenis.hankusic@gmail.com>
2018-08-06 06:30:35 +0200

    Delete keys_prod.js

--- config/keys_prod.js
+++
@@ -1,9 +0,0 @@
-// If you want to use this app you'll need to set up your keys here
-module.exports = {
-  // mLab URI
-  mongoURI:'',
-  // Google Client ID
-  googleClientID:'',
-  // GoogleClient Secret
-  googleClientSecret:''     
-}
