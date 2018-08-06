169ed5e5d496f23cec6e6acc239be44bdbde399e
Dzenis H <dzenis.hankusic@gmail.com>
2018-08-06 05:39:23 +0200

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,9 +1,9 @@
 // If you want to use this app you'll need to set up your keys here
 module.exports = {
   // mLab URI
-  mongoURI:'mongodb://BiggaHD:abc123@ds113482.mlab.com:13482/mini-stories',
+  mongoURI:'',
   // Google Client ID
-  googleClientID:'172280978657-h7hjf5gacgj047gsa3ra7gjfc3s0iprk.apps.googleusercontent.com',
+  googleClientID:'',
   // GoogleClient Secret
-  googleClientSecret:'B-FubQopFFamfNfeOhPD0hXn'     
+  googleClientSecret:''     
 }
